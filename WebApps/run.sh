6c9f0446e9b0445b3fda6b6cd6e22203ed4db66d
gray-area <91284249+gray-area@users.noreply.github.com>
2023-01-25 14:17:12 -0500

    Create run.sh

---
+++ WebApps/run.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+$url=$1
+if [ ! -d "$url" ];then
+  mkdir $url
+fi
+
+if [ ! -d "$url/recon" ];then
+  mkdir $url/recon
+fi
+
+echo "[+] Harvesting subdomains with assetfinder..."
+assetfinder $url >> $url/recon/assets.txt
+cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
+rm $url/recon/assets.txt
