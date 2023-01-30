67be8613d5237d14ec3c9f757890af1abccfbb4d
gray-area <91284249+gray-area@users.noreply.github.com>
2023-01-30 12:32:13 -0500

    Update autopen.sh
    Added color output

--- WebApps/autopen.sh
+++ WebApps/autopen.sh
@@ -10,6 +10,28 @@
 #   subjack
 #   nmap
 
+# Adding color to output
+set +x
+}
+function red(){
+    echo -e "\x1B[31m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[31m $($2) \x1B[0m"
+    fi
+}
+function blue(){
+    echo -e "\x1B[34m $1 \x1B[0m"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[34m $($2) \x1B[0m"
+    fi
+}
+function purple(){
+    echo -e "\x1B[35m $1 \x1B[0m \c"
+    if [ ! -z "${2}" ]; then
+    echo -e "\x1B[35m $($2) \x1B[0m"
+    fi
+}
+
 
 url=$1
 if [ ! -d "$url" ];then
@@ -46,22 +68,22 @@ if [ ! -f "$url/recon/final.txt" ];then
 	touch $url/recon/final.txt
 fi
  
-echo "[+] Harvesting subdomains with assetfinder..."
+purple "[+] Harvesting subdomains with assetfinder..."
 assetfinder $url >> $url/recon/assets.txt
 cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
 rm $url/recon/assets.txt
  
-echo "[+] Double checking for subdomains with amass..."
+purple "[+] Double checking for subdomains with amass..."
 amass enum -d $url >> $url/recon/f.txt
 sort -u $url/recon/f.txt >> $url/recon/final.txt
 rm $url/recon/f.txt
  
-echo "[+] Probing for alive domains..."
+purple "[+] Probing for alive domains..."
 cat $url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe/a.txt
 sort -u $url/recon/httprobe/a.txt > $url/recon/httprobe/alive.txt
 rm $url/recon/httprobe/a.txt
  
-echo "[+] Checking for possible subdomain takeover..."
+purple "[+] Checking for possible subdomain takeover..."
  
 if [ ! -f "$url/recon/potential_takeovers/potential_takeovers.txt" ];then
 	touch $url/recon/potential_takeovers/potential_takeovers.txt
@@ -69,18 +91,18 @@ fi
  
 subjack -w $url/recon/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeovers/potential_takeovers.txt
  
-echo "[+] Scanning for open ports..."
+purple "[+] Scanning for open ports..."
 nmap -iL $url/recon/httprobe/alive.txt -T4 -oA $url/recon/scans/scanned.txt
  
-echo "[+] Scraping wayback data..."
+purple "[+] Scraping wayback data..."
 cat $url/recon/final.txt | waybackurls >> $url/recon/wayback/wayback_output.txt
 sort -u $url/recon/wayback/wayback_output.txt
  
-echo "[+] Pulling and compiling all possible params found in wayback data..."
+purple "[+] Pulling and compiling all possible params found in wayback data..."
 cat $url/recon/wayback/wayback_output.txt | grep '?*=' | cut -d '=' -f 1 | sort -u >> $url/recon/wayback/params/wayback_params.txt
 for line in $(cat $url/recon/wayback/params/wayback_params.txt);do echo $line'=';done
  
-echo "[+] Pulling and compiling js/php/aspx/jsp/json files from wayback output..."
+purple "[+] Pulling and compiling js/php/aspx/jsp/json files from wayback output..."
 for line in $(cat $url/recon/wayback/wayback_output.txt);do
 	ext="${line##*.}"
 	if [[ "$ext" == "js" ]]; then
@@ -111,7 +133,7 @@ rm $url/recon/wayback/extensions/json1.txt
 rm $url/recon/wayback/extensions/php1.txt
 rm $url/recon/wayback/extensions/aspx1.txt
 
-echo "[+] Running gowitness against all compiled domains..."
+purple "[+] Running gowitness against all compiled domains..."
 gowitness -f $url/recon/httprobe/alive.txt -P $url/recon/gowitness --delay 3
 
 
